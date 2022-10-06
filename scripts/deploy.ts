9bdb35de928db439d7c950882fbcb9ec42ddd11b
Quan Pham <doraemonfanclub@gmail.com>
2022-10-06 11:29:10 +0700

    Migrate to hardhat-toolbox

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const TokenplayToken = await ethers.getContractFactory("TokenplayToken");
+  const tToken = await TokenplayToken.deploy();
 
-  await greeter.deployed();
+  await tToken.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Greeter deployed to:", tToken.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
